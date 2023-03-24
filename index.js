a5d0f5b5a1d98fde25b10b9c71b714c4bdfbd8f4
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-03-25 01:17:42 +0500

    update fils

--- index.js
+++ index.js
@@ -1,78 +1,16 @@
-class Book {
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
-    this.books = [];
-    this.booksContainer = document.getElementById('container');
-  }
+import getInputValues from './modules/add-book.js';
+import { goAdd, goContact, goList } from './modules/navigator.js';
+import removeBook from './modules/remove-book.js';
+import localStorageOnLoad from './modules/localstorage-onload.js';
+import { DateTime } from './modules/luxon.js';
 
-  storeBook(book) {
-    localStorage.setItem('bookData', JSON.stringify(book));
-  }
+const now = DateTime.now();
+const clock = document.getElementById('time');
+clock.innerHTML = now.toLocaleString(DateTime.DATETIME_MED);
 
-  getBooks() {
-    if (!localStorage.getItem('bookData')) {
-      this.books = [];
-    } else {
-      this.books = JSON.parse(localStorage.getItem('bookData'));
-    }
-    return this.books;
-  }
-
-  addBook(book) {
-    const recoveredBooks = this.getBooks();
-    recoveredBooks.push(book);
-    this.storeBook(recoveredBooks);
-  }
-
-  removeBook(title, author) {
-    this.books = this.books.filter((book) => book.title !== title || book.author !== author);
-  }
-}
-
-class ShowBooks extends Book {
-  displayBook(book) {
-    const bookCard = document.createElement('div');
-    bookCard.classList.add('book-card');
-
-    const pTitle = document.createElement('p');
-    pTitle.textContent = `"${book.title}" by ${book.author}`;
-
-    const removeBtn = document.createElement('button');
-    removeBtn.textContent = 'Remove';
-
-    bookCard.append(pTitle, removeBtn);
-    this.booksContainer.appendChild(bookCard);
-
-    removeBtn.addEventListener('click', () => {
-      this.removeBook(book.title, book.author);
-      this.storeBook(this.books);
-      this.booksContainer.removeChild(bookCard);
-    });
-  }
-
-  iterateBooks() {
-    this.books = JSON.parse(localStorage.getItem('bookData'));
-    this.books.map((book) => this.displayBook(book));
-  }
-}
-
-const bookForm = document.getElementById('books');
-bookForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
-  if (!title || !author) {
-    alert('Please Enter Title and Author First');
-    return;
-  }
-  const book = new ShowBooks();
-  book.displayBook({ title, author });
-  book.addBook({ title, author });
-
-  title.value = '';
-  author.value = '';
-});
-
-const toDisplay = new ShowBooks();
-toDisplay.iterateBooks();
+window.getInputValues = getInputValues;
+window.goAdd = goAdd;
+window.goContact = goContact;
+window.goList = goList;
+window.removeBook = removeBook;
+localStorageOnLoad();
